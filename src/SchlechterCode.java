e25d566c185772a0def715d1dbcb4c43d94e2d45
rauschmaier <95904777+rauschmaier@users.noreply.github.com>
2023-11-19 11:31:05 +0100

    Create SchlechterCode.java

---
+++ src/SchlechterCode.java
@@ -0,0 +1,18 @@
+public class SchlechterCode4 {
+
+    public static void main(String[] args) {
+        boolean ergebnis = pruefeBedingung(7, 12);
+        if (ergebnis) {
+            System.out.println("Die Bedingung wurde erfüllt!");
+        } else {
+            System.out.println("Die Bedingung wurde nicht erfüllt!");
+        }
+    }
+
+    public static boolean pruefeBedingung(int zahl1, int zahl2) {
+        boolean ergebnis = false;
+        if (zahl1 > zahl2)
+            ergebnis = true;
+        return ergebnis;
+    }
+}
